ab8adc90ea90e0011adfd1e54583a8ee8eaed99e
dem4ply <dem4ply@gmail.com>
2015-06-25 18:13:48 -0500

    [test] se modifico la arquitectura de las pruebas

--- test/ann/synapse.py
+++ test/ann/synapse.py
@@ -3,16 +3,17 @@ import unittest
 
 class Test_synapse(unittest.TestCase):
 	
-	def test_string_output_default(self):
+	def test_string_output_default( self ):
 		synapse = Synapse()
 		self.assertEqual( str( synapse ), "Weigth: 0" )
 		self.assertEqual( repr( synapse ), "W: 0" )
 
-	def test_string_output_params(self):
+	def test_string_output_with_params( self ):
 		synapse = Synapse( weigth = 12 )
 		self.assertEqual( str( synapse ), "Weigth: 12" )
 		self.assertEqual( repr( synapse ), "W: 12" )
 
+	def test_is_synapse_in( self )
+
 if __name__ == '__main__':
     unittest.main()
-
