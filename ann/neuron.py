b1f5a15027cbc303a962b03359cfaaa60aa5d5ea
dem4ply <dem4ply@gmail.com>
2015-06-25 19:13:27 -0500

    backup

--- ann/neuron.py
+++ ann/neuron.py
@@ -1,7 +1,14 @@
-from synapse import Synapse
+from .synapse import Synapse
+from .bypass import Bypass
 
 class Neuron( object ):
 	
-	def __init__(self):
+	def __init__(self, bypass = None):
 		self.synapses_in = []
 		self.synapses_out = []
+		if bypass:
+			self.bypass = bypass
+		else:
+			self.bypass = Bypass()
+
+		self.output = 0
